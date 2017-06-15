acc79de84b488e9362014a0bc15fa32643ed817d
Deepak Kumar Kar <kar.deepak2011@gmail.com>
2017-06-15 12:54:07 +0530

    logging

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.all('/', function(req, res, next) {
+	console.log(req.body);
 	parser.parseCommand(req.body);
     res.send({ "ok": true });
 });
